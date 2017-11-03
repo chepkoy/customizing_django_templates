dbd5949bb1a36cd7ebe9faf2ce010b68cadd49c5
chepkoy <allankiplangat22@gmail.com>
2017-11-03 12:31:57 +0300

    Custom tags

--- courses/templatetags/course_extras.py
+++ courses/templatetags/course_extras.py
@@ -1,5 +1,9 @@
 from django import template
 
+from django.utils.safestring import  mark_safe
+
+import markdown2
+
 from courses.models import Course
 
 register = template.Library()
@@ -24,6 +28,14 @@ def time_estimate(word_count):
     passed-in wordcount.'''
     minutes = round(word_count/20)
     return minutes
+
+
+@register.filter('markdown_to_html')
+def markdown_to_html(markdown_text):
+    '''Converts markdown text to HTML'''
+    html_body = markdown2.markdown(markdown_text)
+    return mark_safe(html_body)
+
 
 
 
